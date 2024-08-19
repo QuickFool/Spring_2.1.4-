a68ed8e450e0c0b98229480f4b50c295ed3f17b6
QuickFool <olvvelold@mail.ru>
2024-08-19 20:25:53 +0300

    init commit

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,8 +1,15 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
 
+    public Deth8 deth8;
     @Override
+    @Autowired
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
     }
